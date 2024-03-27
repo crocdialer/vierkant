7f13732f9a2a65ebc0f56c11049e2ea8404e9b52
crocdialer <crocdialer@gmail.com>
2024-03-27 09:07:55 +0100

    add doc-strings for newly exposed mesh_aabb-routines

--- include/vierkant/mesh_component.hpp
+++ include/vierkant/mesh_component.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <vector>
-#include <set>
 #include <optional>
+#include <set>
+#include <vector>
 #include <vierkant/Mesh.hpp>
 #include <vierkant/object_component.hpp>
 
@@ -20,9 +20,25 @@ struct mesh_component_t
     std::optional<std::set<uint32_t>> entry_indices = {};
 };
 
+/**
+ * @brief   mesh_aabb can be used to generate a combined AABB for all activated mesh-entries,
+ *          optionally applying animation-transforms.
+ *
+ * @param   cmp         a provided vierkant::mesh_component_t
+ * @param   anim_state  optional animation-state
+ * @return  a combined AABB
+ */
 AABB mesh_aabb(const vierkant::mesh_component_t &cmp, const std::optional<vierkant::animation_component_t> &anim_state);
 
+/**
+ * @brief   mesh_sub_aabbs can be used to generate a sequence of sub-AABBs for all activated mesh-entries,
+ *          optionally applying animation-transforms.
+ *
+ * @param   cmp         a provided vierkant::mesh_component_t
+ * @param   anim_state  optional animation-state
+ * @return  a sequence containing sub-AABBs for active mesh-entries
+ */
 std::vector<vierkant::AABB> mesh_sub_aabbs(const vierkant::mesh_component_t &cmp,
                                            const std::optional<vierkant::animation_component_t> &anim_state);
 
-}
+}// namespace vierkant
