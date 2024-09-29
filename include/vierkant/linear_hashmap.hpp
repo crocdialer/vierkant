9269f30029966d5b8445828c0b3a5d181a34f0e9
crocdialer <crocdialer@gmail.com>
2024-09-29 12:11:15 +0200

    generic key/value, resize-function

--- include/vierkant/linear_hashmap.hpp
+++ include/vierkant/linear_hashmap.hpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <shared_mutex>
 
 #include <crocore/utils.hpp>
 #include <vierkant/hash.hpp>
@@ -15,13 +16,23 @@
 namespace vierkant
 {
 
+template<typename K, typename V>
 class linear_hashmap
 {
 public:
-    using key_t = uint64_t;
-    using value_t = uint64_t;
-    using hash_fn = std::function<uint64_t(key_t)>;
+    using key_t = K;
+    using value_t = V;
+    using hash_fn = std::function<uint64_t(uint64_t)>;
+    static_assert(key_t() == key_t(), "key_t not comparable");
+
     linear_hashmap() = default;
+    linear_hashmap(const linear_hashmap &) = delete;
+    linear_hashmap(linear_hashmap &other) : linear_hashmap() { swap(*this, other); };
+    linear_hashmap &operator=(linear_hashmap other)
+    {
+        swap(*this, other);
+        return *this;
+    }
 
     explicit linear_hashmap(uint64_t min_capacity)
         : m_capacity(crocore::next_pow_2(min_capacity)), m_storage(std::make_unique<storage_item_t[]>(m_capacity))
@@ -41,10 +52,12 @@ public:
         for(; ptr != end; ++ptr) { ptr->key = 0; }
     }
 
-    value_t& insert(const key_t &key, value_t value)
+    value_t &put(const key_t &key, value_t value)
     {
         if(m_num_elements >= m_capacity) { throw std::overflow_error("capacity overflow"); }
-        for(uint64_t idx = m_hash_fn(key);; idx++)
+        std::shared_lock lock(m_mutex);
+
+        for(uint64_t idx = hash(key);; idx++)
         {
             idx &= m_capacity - 1;
             auto &item = m_storage[idx];
@@ -71,8 +84,10 @@ public:
 
     [[nodiscard]] std::optional<value_t> get(const key_t &key) const
     {
+        std::shared_lock lock(m_mutex);
+
         if(!m_capacity) { return {}; }
-        for(uint64_t idx = m_hash_fn(key);; idx++)
+        for(uint64_t idx = hash(key);; idx++)
         {
             idx &= m_capacity - 1;
             auto &item = m_storage[idx];
@@ -87,17 +102,56 @@ public:
 
     size_t storage_num_bytes() const { return sizeof(storage_item_t) * m_capacity; }
 
+    void resize(size_t new_capacity)
+    {
+        if(new_capacity > m_capacity)
+        {
+            auto new_linear_hashmap = linear_hashmap(new_capacity);
+            storage_item_t *ptr = m_storage.get(), *end = ptr + m_capacity;
+            for(; ptr != end; ++ptr) { new_linear_hashmap.put(ptr->key, ptr->value); }
+            swap(*this, new_linear_hashmap);
+        }
+    }
+
+    friend void swap(linear_hashmap &lhs, linear_hashmap &rhs)
+    {
+        std::lock(lhs.m_mutex, rhs.m_mutex);
+        std::unique_lock lock_lhs(lhs.m_mutex, std::adopt_lock), lock_rhs(rhs.m_mutex, std::adopt_lock);
+        std::swap(lhs.m_capacity, rhs.m_capacity);
+        lhs.m_num_elements = rhs.m_num_elements.exchange(lhs.m_num_elements);
+        std::swap(lhs.m_storage, rhs.m_storage);
+        std::swap(lhs.m_hash_fn, rhs.m_hash_fn);
+    }
+
 private:
     struct alignas(16) storage_item_t
     {
         std::atomic<key_t> key;
         value_t value{};
     };
-    static_assert(sizeof(storage_item_t) == sizeof(key_t) + sizeof(value_t));
+    static_assert(sizeof(storage_item_t) == sizeof(key_t) + sizeof(value_t),
+                  "alignment/size requirements not met for key_t");
 
+    inline uint64_t hash(const key_t &key) const
+    {
+        constexpr uint32_t num_hashes = sizeof(key_t) / sizeof(uint64_t);
+        constexpr uint32_t num_excess_bytes = sizeof(key_t) % sizeof(uint64_t);
+        auto ptr = reinterpret_cast<const uint64_t *>(&key), end = ptr + num_hashes;
+        size_t h = 0;
+        for(; ptr != end; ++ptr) { vierkant::hash_combine(h, m_hash_fn(*ptr)); }
+        if constexpr(num_excess_bytes)
+        {
+            auto end_u8 = reinterpret_cast<const uint8_t *>(end);
+            uint64_t tail = 0;
+            for(uint32_t i = 0; i < num_excess_bytes; ++i) { tail |= end_u8[i] << (i * 8); }
+            vierkant::hash_combine(h, m_hash_fn(tail));
+        }
+        return h;
+    }
     uint64_t m_capacity{};
     std::atomic<uint64_t> m_num_elements;
     std::unique_ptr<storage_item_t[]> m_storage;
     hash_fn m_hash_fn = vierkant::murmur3_fmix64;
+    mutable std::shared_mutex m_mutex;
 };
 }// namespace vierkant
--- tests/TestLinearHashmap.cpp
+++ tests/TestLinearHashmap.cpp
@@ -1,23 +1,20 @@
-#include <vierkant/linear_hashmap.hpp>
 #include <gtest/gtest.h>
+#include <vierkant/linear_hashmap.hpp>
 
 
 TEST(linear_hashmap, empty)
 {
-    vierkant::linear_hashmap hashmap;
+    vierkant::linear_hashmap<uint64_t, uint64_t> hashmap;
     EXPECT_TRUE(hashmap.empty());
     EXPECT_EQ(hashmap.capacity(), 0);
     EXPECT_EQ(hashmap.storage(), nullptr);
     EXPECT_FALSE(hashmap.storage_num_bytes());
-
-    // empty / no capacity specified -> expect overflow on insert
-    EXPECT_THROW(hashmap.insert(13, 12), std::overflow_error);
 }
 
 TEST(linear_hashmap, basic)
 {
     constexpr uint32_t test_capacity = 100;
-    vierkant::linear_hashmap hashmap(test_capacity);
+    vierkant::linear_hashmap<uint64_t, uint64_t> hashmap(test_capacity);
     EXPECT_TRUE(hashmap.empty());
     EXPECT_TRUE(hashmap.storage());
     EXPECT_TRUE(hashmap.storage_num_bytes());
@@ -29,9 +26,9 @@ TEST(linear_hashmap, basic)
     EXPECT_FALSE(hashmap.contains(13));
     EXPECT_FALSE(hashmap.contains(42));
 
-    auto &v1 = hashmap.insert(69, 99);
+    auto &v1 = hashmap.put(69, 99);
     EXPECT_EQ(v1, 99);
-    auto &v2 = hashmap.insert(13, 12);
+    auto &v2 = hashmap.put(13, 12);
     EXPECT_EQ(v2, 12);
     EXPECT_EQ(hashmap.size(), 2);
 
@@ -39,4 +36,22 @@ TEST(linear_hashmap, basic)
     EXPECT_EQ(hashmap.get(69), 99);
     EXPECT_TRUE(hashmap.contains(13));
     EXPECT_EQ(hashmap.get(13), 12);
+
+    vierkant::linear_hashmap<uint64_t, uint64_t> other_map;
+    other_map = vierkant::linear_hashmap<uint64_t, uint64_t>(19);
+}
+
+TEST(linear_hashmap, resize)
+{
+    vierkant::linear_hashmap<uint64_t, uint64_t> hashmap;
+
+    // empty / no capacity specified -> expect overflow on insert
+    EXPECT_THROW(hashmap.put(13, 12), std::overflow_error);
+
+    // fix by resizing
+    hashmap.resize(17);
+    EXPECT_TRUE(hashmap.empty());
+    EXPECT_TRUE(hashmap.storage());
+    hashmap.put(13, 12);
+    EXPECT_TRUE(hashmap.contains(13));
 }
