74997ab59e27338f61cc2c9d20b5529572c356a5
crocdialer <crocdialer@gmail.com>
2024-10-11 17:38:35 +0200

    adjust meshlet-triangle size, minor shader-refactor

--- shaders/renderer/mesh_task_payload.glsl
+++ shaders/renderer/mesh_task_payload.glsl
@@ -3,7 +3,11 @@
 
 #extension GL_EXT_shader_explicit_arithmetic_types_int8: require
 
-//! TODO: these mesh/task workgroup sizes are still vendor-specific (NV -> 32, AMD -> 64)
+//! TODO: reasonable default setting for nvidia only
+#define MESHLET_MAX_VERTICES 64
+#define MESHLET_MAX_TRIANGLES 84
+
+//! TODO: mesh/task workgroup sizes are still vendor-specific (NV -> 32/32, AMD -> 64/128)
 #define MESH_WORKGROUP_SIZE 32
 #define TASK_WORKGROUP_SIZE 32
 
