a07222ae858d6847a8929450989e6cce427a5958
crocdialer <crocdialer@gmail.com>
2024-10-27 10:32:13 +0100

    fix frustum-cull in y-direction

--- shaders/utils/camera.glsl
+++ shaders/utils/camera.glsl
@@ -16,7 +16,7 @@ struct camera_t
 bool frustum_cull(vec3 center, float radius, vec4 frustum)
 {
     return  !(center.z * frustum[1] - abs(center.x) * frustum[0] > -radius &&
-            center.z * frustum[3] - abs(center.y) * frustum[2] > -radius);
+              -center.z * frustum[3] - abs(center.y) * frustum[2] < radius);
 }
 
 #endif // CAMERA_GLSL
