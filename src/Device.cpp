91b1689538dc2138261a2a3f69833aa68eb78d7a
crocdialer <crocdialer@gmail.com>
2025-09-01 20:35:42 +0200

    come back to VK_KHR_UNIFIED_IMAGE_LAYOUTS_EXTENSION_NAME
    - first need to fix an issue with github-CI wrt. VulkanSDK
    - need to switch from .deb to .tar

--- src/Device.cpp
+++ src/Device.cpp
@@ -215,8 +215,8 @@ Device::Device(const create_info_t &create_info) : m_physical_device(create_info
     // shader barycentric
     check_extension(VK_KHR_FRAGMENT_SHADER_BARYCENTRIC_EXTENSION_NAME);
 
-    // KHR_unified_image_layouts
-    check_extension(VK_KHR_UNIFIED_IMAGE_LAYOUTS_EXTENSION_NAME);
+    // TODO: KHR_unified_image_layouts
+    // check_extension(VK_KHR_UNIFIED_IMAGE_LAYOUTS_EXTENSION_NAME);
 
     if(create_info.use_validation) { check_extension(VK_KHR_SHADER_RELAXED_EXTENDED_INSTRUCTION_EXTENSION_NAME); }
 
