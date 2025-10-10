61929140537071a309149976a5a69da0e66ee6d5
crocdialer <crocdialer@gmail.com>
2025-10-10 18:32:14 +0200

    swapcain another correction

--- src/SwapChain.cpp
+++ src/SwapChain.cpp
@@ -72,16 +72,12 @@ VkPresentModeKHR choose_swap_present_mode(const std::vector<VkPresentModeKHR> &m
     {
         if(use_vsync)
         {
-            if(m == VK_PRESENT_MODE_MAILBOX_KHR) { best_mode = m; }
-            // else if(m == VK_PRESENT_MODE_FIFO_LATEST_READY_KHR) { best_mode = m; }
+            // if(m == VK_PRESENT_MODE_FIFO_LATEST_READY_KHR) { best_mode = m; }
         }
         else
         {
-            if(m == VK_PRESENT_MODE_IMMEDIATE_KHR)
-            {
-                best_mode = m;
-                break;
-            }
+            if(m == VK_PRESENT_MODE_IMMEDIATE_KHR) { best_mode = m; }
+            else if(m == VK_PRESENT_MODE_MAILBOX_KHR) { best_mode = m; }
         }
     }
     return best_mode;
