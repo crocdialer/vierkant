647cb05817e73fcec4627a1a3e932457680c71cd
crocdialer <crocdialer@gmail.com>
2026-02-01 14:21:36 +0100

    Fix a copy/lifetime issue introduced in last commit

--- src/PBRDeferred.cpp
+++ src/PBRDeferred.cpp
@@ -764,8 +764,10 @@ vierkant::Framebuffer &PBRDeferred::geometry_pass(cull_result_t &cull_result)
         else if(params.num_draws && (!frame_context.dirty_drawable_indices.empty() ||
                                      !frame_context.mesh_compute_result.vertex_buffer_offsets.empty()))
         {
+            constexpr size_t stride = sizeof(Rasterizer::mesh_draw_t);
             constexpr size_t staging_stride = 2 * sizeof(matrix_struct_t);
 
+            std::vector<VkDeviceAddress> vertex_buffer_addresses;
             std::vector<vierkant::matrix_struct_t> matrix_data(2 * frame_context.dirty_drawable_indices.size());
             std::vector<vierkant::material_struct_t> material_data(frame_context.dirty_drawable_indices.size());
             std::vector<vierkant::staging_copy_info_t> staging_copies;
@@ -783,7 +785,6 @@ vierkant::Framebuffer &PBRDeferred::geometry_pass(cull_result_t &cull_result)
 
                     if(flag & flag_component_t::DIRTY_TRANSFORM)
                     {
-                        constexpr size_t stride = sizeof(Rasterizer::mesh_draw_t);
                         vierkant::staging_copy_info_t copy_transform = {};
                         copy_transform.num_bytes = staging_stride;
                         copy_transform.data = matrix_data.data() + 2 * i;
@@ -822,7 +823,6 @@ vierkant::Framebuffer &PBRDeferred::geometry_pass(cull_result_t &cull_result)
             // vertex-buffers update from animation
             if(!frame_context.mesh_compute_result.vertex_buffer_offsets.empty())
             {
-                std::vector<VkDeviceAddress> vertex_buffer_addresses;
                 vertex_buffer_addresses.reserve(frame_context.mesh_compute_result.vertex_buffer_offsets.size());
                 std::unordered_set<uint32_t> mesh_indices;
 
@@ -833,8 +833,9 @@ vierkant::Framebuffer &PBRDeferred::geometry_pass(cull_result_t &cull_result)
 
                     for(uint32_t idx: frame_context.cull_result.object_id_to_drawable_indices[obj_id])
                     {
-                        if(uint32_t vertex_buffer_index = params.mesh_draws_host[idx].vertex_buffer_index;
-                           !mesh_indices.contains(vertex_buffer_index))
+                        uint32_t vertex_buffer_index = params.mesh_draws_host[idx].vertex_buffer_index;
+
+                        if(!mesh_indices.contains(vertex_buffer_index))
                         {
                             vierkant::staging_copy_info_t copy_vertex_address = {};
                             copy_vertex_address.num_bytes = sizeof(VkDeviceAddress);
