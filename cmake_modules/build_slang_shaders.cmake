e7887b834b4ef7ecf70b2e567b4526e8ce0a9ac5
Fabian Schmidt <crocdialer@gmail.com>
2026-03-07 11:03:07 +0100

    setup cmake-boilerplate to compile slang-shaders (#147)
    * setup cmake-boilerplate to compile slang-shaders
    - basically c/p the existing glsl approach for starters
    
    * switch to different github-action for providing vulkan-SDK
    - main reaosn is old actions seems to not provide slangc

---
+++ cmake_modules/build_slang_shaders.cmake
@@ -0,0 +1,23 @@
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake_modules ${CMAKE_MODULE_PATH})
+include(vierkant_utils)
+
+if (NOT DEFINED SLANG_SOURCE_ROOT)
+    set(SLANG_SOURCE_ROOT "shaders/slang")
+endif ()
+
+if (NOT DEFINED TARGET_NAME)
+    set(TARGET_NAME ${PROJECT_NAME})
+endif ()
+
+if (NOT DEFINED SPIRV_OUTPUT_DIR)
+    set(SPIRV_OUTPUT_DIR "${PROJECT_BINARY_DIR}")
+endif ()
+
+if (NOT DEFINED SOURCE_OUTPUT_DIR)
+    set(SOURCE_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+endif ()
+
+# set slang compiler
+find_program(SLANG_COMPILER "slangc" REQUIRED)
+
+stringify_slang_shaders(${SLANG_SOURCE_ROOT} ${TARGET_NAME} ${SLANG_COMPILER} ${SPIRV_OUTPUT_DIR} ${SOURCE_OUTPUT_DIR})
