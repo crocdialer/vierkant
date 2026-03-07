a8f5de6c57af46f688b24e3d87367e458ab272e5
crocdialer <crocdialer@gmail.com>
2026-03-07 20:02:06 +0100

    correct some tests

--- tests/TestSpirvReflect.cpp
+++ tests/TestSpirvReflect.cpp
@@ -9,8 +9,10 @@ TEST(TestSpirvReflect, GroupCount)
     vulkan_test_context_t test_context;
 
     // overlay-compute
-    glm::uvec3 local_sizes;
-    auto shader_stage = vierkant::create_shader_module(test_context.device,
-                                                       vierkant::shaders::renderer::object_overlay_comp, &local_sizes);
+    auto shader_stage = vierkant::create_shader_module(vierkant::shaders::renderer::object_overlay_comp);
+    EXPECT_TRUE(!shader_stage.spirv.empty());
+    EXPECT_TRUE(shader_stage.entry_points.contains(VK_SHADER_STAGE_COMPUTE_BIT));
+    EXPECT_TRUE(shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).group_count);
+    glm::uvec3 local_sizes = *shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).group_count;
     EXPECT_EQ(local_sizes, glm::uvec3(32, 32, 1));
 }
