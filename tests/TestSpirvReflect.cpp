53b2e8c2bf40df5fda3cda715403bd8b0fe421ef
crocdialer <crocdialer@gmail.com>
2026-03-11 21:57:24 +0100

    Fix test

--- tests/TestSpirvReflect.cpp
+++ tests/TestSpirvReflect.cpp
@@ -10,9 +10,10 @@ TEST(TestSpirvReflect, GroupCount)
 
     // overlay-compute
     auto shader_stage = vierkant::create_shader_module(vierkant::shaders::renderer::object_overlay_comp);
-    EXPECT_TRUE(!shader_stage.spirv.empty());
+    EXPECT_TRUE(shader_stage.create_info.codeSize);
     EXPECT_TRUE(shader_stage.entry_points.contains(VK_SHADER_STAGE_COMPUTE_BIT));
-    EXPECT_TRUE(shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).group_count);
-    glm::uvec3 local_sizes = *shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).group_count;
+    EXPECT_FALSE(shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).empty());
+    EXPECT_TRUE(shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).front().group_count);
+    glm::uvec3 local_sizes = *shader_stage.entry_points.at(VK_SHADER_STAGE_COMPUTE_BIT).front().group_count;
     EXPECT_EQ(local_sizes, glm::uvec3(32, 32, 1));
 }
